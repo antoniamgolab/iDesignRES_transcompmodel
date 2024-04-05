58731f184a2d711a30979050aa9a22f570e6c695
antoniamgolab <antonia.golab@tuwien.ac.at>
2024-04-05 11:23:23 +0200

    model extension

--- _functions.jl
+++ _functions.jl
@@ -1,1 +1,76 @@
 # set generation
+function generate_set_K_r(R, K, K_r)
+    output_set = Set()
+    for r in R
+        for k in K_r[r]
+            push!(output_set, (r, k))
+        end
+    end 
+    return output_set
+end
+
+function generate_yktg(Y, K, T, G)
+    output_set = Set()
+    for y in Y
+        for k in K
+            for t in T
+                for g in G
+                    push!(output_set, (y, k, t, g))
+                end
+            end
+        end
+    end 
+    return output_set
+end
+
+function generate_yrtg(Y, R, T, G)
+    output_set = Set()
+    for y in Y
+        for r in R
+            for t in T
+                for g in G
+                    push!(output_set, (y, r, t, g))
+                end
+            end
+        end
+    end 
+    return output_set
+end
+
+function generate_yr(Y, R)
+    output_set = Set()
+    for y in Y
+        for r in R
+            push!(output_set, (y, r))
+        end
+    end
+    return output_set
+end
+
+function generate_later_yrtg(Y, R, T, G)
+    output_set = Set()
+    for y in Y
+        for r in R
+            for t in T
+                for g in G
+                    if y > 1
+                        push!(output_set, (y, r, t, g))
+                    end
+                end
+            end
+        end
+    end
+    return output_set
+end
+
+function generate_rtg(R, T, G)
+    output_set = Set()
+    for r in R
+        for t in T
+            for g in G
+                push!(output_set, (r, t, g))
+            end
+        end
+    end
+    return output_set
+end
--- _optimize.jl
+++ _optimize.jl
@@ -1,48 +1,84 @@
+println("Running the program ...")
+
 using JuMP, Gurobi
 include("_functions.jl")
 
 # preliminary parametrization
+Y = 2 # year 
 R = 1 # origin-destination pair
 K = 1 # connection between an origin-destination pair
 T = 1 # technology
-N = 1 # node 
 G = 1 # vehicle generation
+N = 3 # node 
+E = 2 # edge
+
+y_vec = collect(1:Y)
+r_vec = collect(1:R)
+k_vec = collect(1:K)
+t_vec = collect(1:T)
+n_vec = collect(1:N)
+g_vec = collect(1:G)
 
 # subsets 
-K_r = Dict(1 => [1])
+K_r = Dict(1 => [1]) 
+
+# geometries
+nodes = Set([1, 2, 3])
+edges = Set([4, 5])
+
+# routes
+route = Dict(1 => [1, 4, 5, 3])
 
 # parameters
-F = [2000] # r 
-L_node = [20] # nk; route length within a note 
-W = [10.63] # t 
-L_a = [120000]
+F = Dict((1, 1) => 2000, (2, 1) => 2000) # y, r 
+L_node_edges = Dict((1, 1) => 200, (2, 1) => 200, (3, 1) => 200, (4, 1) => 5000, (5, 1) => 5000) # (e/n)k; route length within a note 
+W = Dict((1, 1) => 10.63) # t, g 
+L_a = Dict((1, 1) => 1200000)# t, g
 alpha = 0.5
 beta = 0.5
+h_init = Dict((1, 1, 1) => 1) # rtg
 
 # cost parameters
+c_transport =  Dict((1,1,1,1) => 10, (2,1,1,1) => 10) # yktg
+c_h = Dict((1, 1, 1) => 2, (2, 1, 1) => 2) # ytg 
 
-
+# model initialization
 model = Model(Gurobi.Optimizer)
 
+# add the dimensions
+# initializing sets 
+yktg_set = generate_yktg(y_vec, k_vec, t_vec, g_vec)
+yrtg_set = generate_yrtg(y_vec, r_vec, t_vec, g_vec)
+yr_set = generate_yr(y_vec, r_vec)
+rtg_set = generate_rtg(r_vec, t_vec, g_vec)
+later_yrtg_set = generate_later_yrtg(y_vec, r_vec, t_vec, g_vec)
+
 # setting decision variables 
-@variable(model, f[1:K, 1:T. 1:G] => 0)
-@variable(model, h[1:R, 1:T, 1:G] => 0)
-@variable(model, h_plus[1:R, 1:T, 1:G] => 0)
-@variable(model, h_minus[1:R, 1:T, 1:G] => 0)
+@variable(model, f[yktg_set] >= 0)
+@variable(model, h[yrtg_set] >= 0)
+@variable(model, h_plus[yrtg_set] >= 0)
+@variable(model, h_minus[yrtg_set] >= 0)
 
 # setting constraints
-@constraint(model, )
+# constraint for demand coverage 
+@constraint(model, [subset in yr_set], sum(f[(subset[1], k, t, g)] for k in K_r[subset[2]] for g in g_vec for t in t_vec) == F[(subset[1], subset[2])])
 
+# ----
+# vehicle fleet constraints
+# ---
+@constraint(model, [subset in yrtg_set], h[subset] >= sum((sum(L_node_edges[(route_element, k)] for route_element in route[k])/(W[(subset[3], subset[4])] * L_a[(subset[3], subset[4])])) * f[(subset[1], k, subset[3], subset[4])] for k in K_r[subset[2]]))
+# t = 1
+@constraint(model, [subset in rtg_set], h[(1, subset[1], subset[2], subset[3])] == h_init[subset] + h_plus[(1, subset[1], subset[2], subset[3])] - h_minus[(1, subset[1], subset[2], subset[3])])
+# t > 1
+@constraint(model, [subset in later_yrtg_set], h[subset] == h[(subset[1]-1, subset[2], subset[3], subset[4])] + h_plus[subset] - h_minus[subset])
 
+# ---
+# objective
+# ---
+@objective(model, Min, sum(c_transport[subset]*f[subset] for subset in yktg_set) + sum(h_plus[subset]*c_h[(subset[1], subset[3], subset[4])] for subset in yrtg_set))
 
-set_attribute(model, "TimeLimit", 100)
-
-# @variable(model, x >= 0)
-# @variable(model, 0 <= y <= 3)
-# @objective(model, Min, 12x + 20y)
-# @constraint(model, c1, 6x + 8y >= 100)
-# @constraint(model, c2, 7x + 12y >= 120)
-# print(model)
-
-
-
+# ---
+# solution
+# ---
+optimize!(model)
+solution_summary(model)
