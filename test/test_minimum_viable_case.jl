6e825e4bfd029069790b3534e873ecf5ca891811
Antonia Golab <antonia.golab@tuwien.ac.at>
2025-03-31 16:59:58 +0200

    correcting testing of minimal viable case

--- test/test_minimum_viable_case.jl
+++ test/test_minimum_viable_case.jl
@@ -387,6 +387,6 @@ end
         round(
             sum(q_fuel_infr_plus_dict[(y, 1, 1)] for f ∈ fuel_list for y ∈ y_init:Y_end),
             digits = 2,
-        ) == 0.14
+        ) == 0.11
     )
 end
