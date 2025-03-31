880a5fcfebe9e9d8d2385ba31330b102d7342810
Antonia Golab <antonia.golab@tuwien.ac.at>
2025-03-31 17:01:12 +0200

    upgrading testing add @testset around the subtest to display one final table with results of tests #41

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,27 +3,29 @@ using JuMP
 import HiGHS
 using Test
 
-@testset "Basic function | Technology shift" begin
-    @testset "Tech shift test" begin
-        include("test_techshift.jl")
+@testset "Model testing" begin
+    @testset "Basic function | Technology shift" begin
+        @testset "Tech shift test" begin
+            include("test_techshift.jl")
+        end
     end
-end
 
-@testset "Basic function | Mode shift" begin
-    @testset "Mode shift test" begin
-        include("test_mode_shift.jl")
+    @testset "Basic function | Mode shift" begin
+        @testset "Mode shift test" begin
+            include("test_mode_shift.jl")
+        end
     end
-end
 
-@testset "Minimum viable case" begin
-    @testset "Vehicle stock shift test" begin
-        include("test_minimum_viable_case.jl")
+    @testset "Minimum viable case" begin
+        @testset "Vehicle stock shift test" begin
+            include("test_minimum_viable_case.jl")
+        end
     end
-end
 
-@testset "Minimum viable case | Parameter testing" begin
-    +
-    @testset "Model robustness" begin
-        include("test_model_robustness.jl")
-    end
+    @testset "Minimum viable case | Parameter testing" begin
+        +
+        @testset "Model robustness" begin
+            include("test_model_robustness.jl")
+        end
+    end 
 end
